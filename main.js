404cf3b75710c85b324ce9dc82d9dbba8a4a7b77
zengqueling <62401759+zengqueling@users.noreply.github.com>
2024-10-18 18:31:47 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const saveButton = document.getElementById('save-btn');
 
 const defaultContent = `# 欢迎使用令爷 Markdown 一页笔记
 
+
+
+
 ## 任务列表示例
 - [x] 创建 Markdown 笔记应用
 - [ ] 添加更多功能
@@ -12,11 +15,14 @@ const defaultContent = `# 欢迎使用令爷 Markdown 一页笔记
 ## 超链接示例
 查看 [Markdown 指南](https://www.markdown.xyz/basic-syntax/) 了解更多 Markdown 语法！
 
+
 ## 表格示例
 | 列1 | 列2 | 列3 |
 |-----|-----|-----|
 | 内容1 | 内容2 | 内容3 |
 | 内容4 | 内容5 | 内容6 |
+
+
 
 ## Mermaid 图表示例
 \`\`\`mermaid
@@ -27,6 +33,15 @@ graph TD
     C --> D
 \`\`\`
 
+
+
+
+
+
+
+
+
+
 ## 代码块示例（Python）
 \`\`\`python
 def hello_world():
@@ -35,7 +50,10 @@ def hello_world():
 hello_world()
 \`\`\`
 
-开始编辑这个笔记或创建你自己的笔记吧！`;
+
+开始编辑这个笔记或创建你自己的笔记吧！
+
+`;
 
 // 配置 marked
 marked.setOptions({
